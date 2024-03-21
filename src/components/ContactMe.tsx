4a03426cf594bbba0e5e171e2d95571388a7a685
Koushik <programmer.koushik@gmail.com>
2024-03-21 21:11:15 +0530

    contact me

---
+++ src/components/ContactMe.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+type Props = {};
+
+export default function ContactMe({}: Props) {
+  return (
+    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Contact
+      </h3>
+
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center">Lets Talk.</h4>
+
+        <form className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex space-x-2">
+            <input placeholder="Name" className="contactInput" type="text" />
+            <input placeholder="Email" className="contactInput" type="email" />
+          </div>
+          <input placeholder="Subject" className="contactInput" type="text" />
+          <textarea placeholder="Message" className="contactInput" />
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
