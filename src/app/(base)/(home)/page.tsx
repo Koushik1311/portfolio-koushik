c57e5495b6bf5e687aa38c23e0b539a3bd248a1a
Koushik <programmer.koushik@gmail.com>
2024-03-21 21:11:30 +0530

    update

--- src/app/(base)/(home)/page.tsx
+++ src/app/(base)/(home)/page.tsx
@@ -1,7 +1,10 @@
-import About from "@/app/components/About";
-import Header from "@/app/components/Header";
-import Hero from "@/app/components/Hero";
-import WorkExperience from "@/app/components/WorkExperience";
+import About from "@/components/About";
+import ContactMe from "@/components/ContactMe";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+import WorkExperience from "@/components/WorkExperience";
 import Image from "next/image";
 
 export default function Home() {
@@ -26,10 +29,19 @@ export default function Home() {
       </section>
 
       {/* Skills */}
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
 
       {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contact Me */}
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
     </main>
   );
 }
